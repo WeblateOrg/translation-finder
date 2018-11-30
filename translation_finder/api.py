c88e637c468597a7842d23b23e83d883394af1c9
Michal Čihař <michal@cihar.com>
2018-11-30 17:24:35 +0100

    Use https in license link
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- translation_finder/api.py
+++ translation_finder/api.py
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """Highlevel API for translation-finder."""
 from __future__ import unicode_literals, absolute_import, print_function
