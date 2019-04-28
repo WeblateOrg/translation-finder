a1571eb5c1b4416e2ced4776c065599262262632
Michal Čihař <michal@cihar.com>
2019-04-28 20:04:43 +0200

    Add module docs
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- translation_finder/__init__.py
+++ translation_finder/__init__.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+"""Translation finder, a module to locate translatable files in
+a filesystem."""
 from __future__ import unicode_literals, absolute_import
 
 from .finder import Finder
